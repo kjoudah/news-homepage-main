3440a3ca239aba4c7d9f7df933b5a9510fd64509
Karam <karamjoudah@hotmail.com>
2023-05-08 00:40:18 -0400

    Looking like the finished product, need to thoroughly test

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,13 +1,44 @@
+import { useState } from 'react';
 import logo from './../../assets/images/logo.svg';
 import './Nav.scss';
 
-function Nav() {
+const MobileHeader = () => {
+  const [open, setOpen] = useState(false);
+
   return (
-    <header className="header">
-      <img src={logo} alt="logo" />
-      <nav>
-        <ul className="nav">
-          <li className="nav-item"> Home</li>
+    <header>
+      <nav className="nav nav-mobile">
+        {open ? (
+          <div onClick={() => setOpen(false)} className="backdrop"></div>
+        ) : (
+          <></>
+        )}
+        <img src={logo} alt="logo" />
+        <div className="mobile-menu">
+          <ul className={open ? 'mobile-nav open' : 'mobile-nav'}>
+            <li className="nav-item mobile">Home</li>
+            <li className="nav-item mobile">New</li>
+            <li className="nav-item mobile">Popular</li>
+            <li className="nav-item mobile">Trending</li>
+            <li className="nav-item mobile">Categories</li>
+          </ul>
+        </div>
+        <button
+          onClick={() => setOpen((prev) => !prev)}
+          className={open ? 'menu-toggle open' : 'menu-toggle'}
+        ></button>
+      </nav>
+    </header>
+  );
+};
+
+const DesktopNav = () => {
+  return (
+    <header>
+      <nav className="nav nav-desktop">
+        <img src={logo} alt="logo" />
+        <ul className="desktop-nav">
+          <li className="nav-item">Home</li>
           <li className="nav-item">New</li>
           <li className="nav-item">Popular</li>
           <li className="nav-item">Trending</li>
@@ -15,6 +46,55 @@ function Nav() {
         </ul>
       </nav>
     </header>
+  );
+};
+
+function Nav() {
+  // const [open, setOpen] = useState(false);
+
+  return (
+    <>
+      <MobileHeader />
+      <DesktopNav />
+    </>
+    // <>
+    //   <header>
+    //     <nav className="nav nav-mobile">
+    //       {open ? (
+    //         <div onClick={() => setOpen(false)} className="backdrop"></div>
+    //       ) : (
+    //         <></>
+    //       )}
+    //       <img src={logo} alt="logo" />
+    //       <div className="mobile-menu">
+    //         <ul className={open ? 'mobile-nav open' : 'mobile-nav'}>
+    //           <li className="nav-item mobile">Home</li>
+    //           <li className="nav-item mobile">New</li>
+    //           <li className="nav-item mobile">Popular</li>
+    //           <li className="nav-item mobile">Trending</li>
+    //           <li className="nav-item mobile">Categories</li>
+    //         </ul>
+    //       </div>
+    //       <button
+    //         onClick={() => setOpen((prev) => !prev)}
+    //         className={open ? 'menu-toggle open' : 'menu-toggle'}
+    //       ></button>
+    //     </nav>
+    //   </header>
+
+    //   <header>
+    //     <nav className="nav nav-desktop">
+    //       <img src={logo} alt="logo" />
+    //       <ul className="desktop-nav">
+    //         <li className="nav-item">Home</li>
+    //         <li className="nav-item">New</li>
+    //         <li className="nav-item">Popular</li>
+    //         <li className="nav-item">Trending</li>
+    //         <li className="nav-item">Categories</li>
+    //       </ul>
+    //     </nav>
+    //   </header>
+    // </>
   );
 }
 
